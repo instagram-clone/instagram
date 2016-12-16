6a5b2cfcf3e163386c725068a88264ac39ddc07c
mr433 <max.rodewald@gmail.com>
2016-12-16 16:45:16 -0700

    worked on login page

--- public/views/Login/LoginView.js
+++ public/views/Login/LoginView.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
+import Footer from './Footer';
 
 export default class LoginView extends React.Component{
   render(){
     return(
       <div>
         <h1>This is the login view!</h1>
+        <LoginForm />
+		<SignupForm />
+		<Footer />
       </div>
     )
   }
