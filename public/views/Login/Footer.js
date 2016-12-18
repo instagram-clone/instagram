465a25a8b8e66fa85ed56a7764eb64ea3c180194
mr433 <max.rodewald@gmail.com>
2016-12-18 16:34:34 -0700

    did css for the login page and changed function calls to use .bind()

--- public/views/Login/Footer.js
+++ public/views/Login/Footer.js
@@ -3,20 +3,23 @@ import React from 'react';
 export default class Footer extends React.Component{
 	render(){
 		return(
-			<div>
+			<footer>
 				<ul>
-					<li>About us</li>
-					<li>Support</li>
-					<li>Blog</li>
-					<li>Press</li>
-					<li>API</li>
-					<li>Jobs</li>
-					<li>Privacy</li>
-					<li>terms</li>
-					<li>directory</li>
-					<li>Language</li>
+					<a href='#'><li>About us</li></a>
+					<a href='#'><li>Support</li></a>
+					<a href='#'><li>Blog</li></a>
+					<a href='#'><li>Press</li></a>
+					<a href='#'><li>API</li></a>
+					<a href='#'><li>Jobs</li></a>
+					<a href='#'><li>Privacy</li></a>
+					<a href='#'><li>terms</li></a>
+					<a href='#'><li>directory</li></a>
+					<a href='#'><li>Language</li></a>
+					<div className='copyright'>
+						© 2016 INSTAGRAM
+					</div>
 				</ul>
-			</div>
+			</footer>
 		)
 	}
 }
