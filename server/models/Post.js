b64e12f5844ed355591d8e575b1dd9c356ac893a
Jefferson Snow <socialhit@gmail.com>
2016-12-19 16:14:17 -0700

    changed the schema to add description and to properly create likes and comments arrays

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
+const User = require('./User');
 const Post = mongoose.Schema({
-  author: {type: String, required: true},
+  author: {type: mongoose.Schema.Types.ObjectId, ref:"User"},
   photourl: {type: String, required: true},
   timestamp: {type: Date, required: true},
-  likes: {type: Array, required: false},
-  comments: {type: Array, required: false},
+  likes: [
+    {
+      user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+    }
+  ],
+  comments: [
+      {
+        user: {type: mongoose.Schema.Types.ObjectId, ref: "User"},
+        comment: {type: String}
+      }
+    ],
+  description: {type: String, required: false},
 });
 module.exports = mongoose.model('Post', Post);
