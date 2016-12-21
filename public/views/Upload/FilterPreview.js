93137a5e5daceed397aaf1584850cfb547e51708
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-12-21 11:03:51 -0700

    search skeleton using photogrid component from profile

---
+++ public/views/Upload/FilterPreview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class FilterPreview extends React.Component{
+    render(){
+        return(
+        <div>
+            <img />
+            <h1>filter goes on top of image</h1>
+        </div>
+        )
+    }
+}
