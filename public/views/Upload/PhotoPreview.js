2c666e2f0e606c8733ff60eaaafb8dd6d335a78f
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-12-20 17:53:30 -0700

    able to post and get photo from s3

--- public/views/Upload/PhotoPreview.js
+++ public/views/Upload/PhotoPreview.js
@@ -4,8 +4,9 @@ export default class PhotoPreview extends React.Component{
     render(){
         return(
             <div className="preview">
-                <img />
-                <h1>Preview of the photo</h1>
+            {
+                this.props.url ? <img src={this.props.url}/> : <h1>Preview of the photo</h1>
+            }
             </div>
         )
     }
--- public/views/Upload/UploadView.js
+++ public/views/Upload/UploadView.js
@@ -5,16 +5,38 @@ import ReactS3Uploader from 'react-s3-uploader';
 import PhotoPreview from './PhotoPreview';
 
 export default class UploadView extends React.Component{
-  onProgress(){
-    
-  }
-  onError(){
+  constructor(){
+    super();
+    this.state = {
 
+    }
   }
-  handleFinish(){
-    axios.get('/s3/img/(.*)').then( data => {
-      console.log(data);
-    });
+  handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+    console.log(file);
+
+    reader.onloadend = (loadEvent) => {
+      this.setState({
+        file: file,
+        imgUrl: reader.result
+      });
+      const newImage = {
+        imageName: file.name,
+        imageBody: loadEvent.target.result,
+        imageExtension: file.type
+      }
+      axios.post('/api/s3', newImage).then((response) => {
+        console.log(response);
+        this.setState({
+          imgUrl: response.data.Location,
+        })
+      });
+    }
+
+    reader.readAsDataURL(file)
   }
   render(){
     return(
@@ -23,15 +45,11 @@ export default class UploadView extends React.Component{
         <div className="upload">
           <br />
           <h1>Upload</h1>
-          <br />
           <br/>
-            <ReactS3Uploader id="picinput"
-              signingUrl='/s3/sign'
-              accept="image/*"
-              onFinish={this.handleFinish.bind(this)}
-            />
+          <br/>
+            <input type="file" onChange={this.handleImageChange.bind(this)}/>
         </div>
-        <PhotoPreview />
+        <PhotoPreview url={this.state.imgUrl}/>
       </div>
     )
   }
