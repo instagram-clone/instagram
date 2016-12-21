5665e5ee31589460fd86f8c11d47832c894f95ee
mr433 <max.rodewald@gmail.com>
2016-12-21 10:55:20 -0700

    Added endpoint for posting photos. Basic styling for upload page.

--- public/views/Upload/UploadView.js
+++ public/views/Upload/UploadView.js
@@ -3,54 +3,77 @@ import Nav from '../Nav/Nav';
 import axios from 'axios';
 import ReactS3Uploader from 'react-s3-uploader';
 import PhotoPreview from './PhotoPreview';
+import {getLoggedInUser} from '../../utils/getLoggedInUser';
 
-export default class UploadView extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-
+export default class UploadView extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
     }
-  }
-  handleImageChange(e) {
-    e.preventDefault();
+    handleCaptionChange(e) {
+        this.setState({caption: e.target.value});
+        console.log(this.state.caption);
+    }
+    handleImageChange(e) {
+        e.preventDefault();
 
-    let reader = new FileReader();
-    let file = e.target.files[0];
-    console.log(file);
+        let reader = new FileReader();
+        let file = e.target.files[0];
+        console.log(file);
 
-    reader.onloadend = (loadEvent) => {
-      this.setState({
-        file: file,
-        imgUrl: reader.result
-      });
-      const newImage = {
-        imageName: file.name,
-        imageBody: loadEvent.target.result,
-        imageExtension: file.type
-      }
-      axios.post('/api/s3', newImage).then((response) => {
-        console.log(response);
         this.setState({
-          imgUrl: response.data.Location,
+            date: file.lastModifiedDate
+        });
+
+        reader.onloadend = (loadEvent) => {
+            this.setState({file: file, imgUrl: reader.result});
+            const newImage = {
+                imageName: file.name,
+                imageBody: loadEvent.target.result,
+                imageExtension: file.type
+            }
+            axios.post('/api/s3', newImage).then((response) => {
+                console.log(response);
+                this.setState({
+                    imgUrl: response.data.Location,
+                });
+            });
+        }
+
+        reader.readAsDataURL(file)
+    }
+    handleSubmit(e){
+        this.setState({
+            author: getLoggedInUser().username
         })
-      });
+
+        //had to use settimeout because otherwise the
+        //post would be made before the author name was set
+        window.setTimeout(() => {
+            axios.post('/api/postPhoto', {
+                data : this.state
+            }).then(response => {
+                if(response.status === 200){
+                    window.location.href = '/#/feed';
+                }
+            });
+        }, 1000);
     }
 
-    reader.readAsDataURL(file)
-  }
-  render(){
-    return(
-      <div>
-        <Nav/>
-        <div className="upload">
-          <br />
-          <h1>Upload</h1>
-          <br/>
-          <br/>
-            <input type="file" onChange={this.handleImageChange.bind(this)}/>
-        </div>
-        <PhotoPreview url={this.state.imgUrl}/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Nav/>
+                <div className='uploadView'>
+                    <div className="upload">
+                        <div className='uploadHeader'>Upload</div>
+                        <input type="file" onChange={this.handleImageChange.bind(this)}/>
+                    </div>
+                    <PhotoPreview url={this.state.imgUrl}/>
+                    <input className='caption' placeholder='caption' onChange={this.handleCaptionChange.bind(this)}/>
+                    <button className='button' onClick={this.handleSubmit.bind(this)}>Post</button>
+                </div>
+            </div>
+        )
+    }
 }
