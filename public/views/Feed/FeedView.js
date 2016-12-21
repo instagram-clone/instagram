0bce659c76d8af9b8854eacc07c642f0497d30ca
mr433 <max.rodewald@gmail.com>
2016-12-20 17:08:49 -0700

    Adding welcome card and card that prompts to add profile pic. Fixed setState error on the carousel

--- public/views/Feed/FeedView.js
+++ public/views/Feed/FeedView.js
@@ -2,12 +2,17 @@ import React from 'react';
 import Nav from '../Nav/Nav';
 import PhotoCard from './FeedPhoto/PhotoCard';
 import Footer from '../Login/Footer';
+import WelcomeCard from './welcomeCard';
+import AddProfileImg from './AddProfileImg';
+
 export default class FeedView extends React.Component {
     render() {
         return (
             <div className='feedView'>
-                <Nav/>
-                <PhotoCard/>
+                <Nav />
+                <AddProfileImg />
+                <WelcomeCard />
+                <PhotoCard />
                 <Footer />
             </div>
         )
