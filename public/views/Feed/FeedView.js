11ab692a170680c725d70cd9195986814d14183b
mr433 <max.rodewald@gmail.com>
2016-12-21 18:05:46 -0700

    Made feed that gets all posts from users that the logged in user follows. Fixed bug in landing page carousel. Css changes on upload page

--- public/views/Feed/FeedView.js
+++ public/views/Feed/FeedView.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import {getLoggedInUser} from '../../utils/getLoggedInUser';
+
 import Nav from '../Nav/Nav';
 import PhotoCard from './FeedPhoto/PhotoCard';
 import Footer from '../Login/Footer';
@@ -6,14 +9,36 @@ import WelcomeCard from './welcomeCard';
 import AddProfileImg from './AddProfileImg';
 
 export default class FeedView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            feedSet: false
+        }
+    }
+    componentDidMount() {
+        let feed = [];
+        axios.get('/api/feed?username=' + getLoggedInUser().username).then(response => {
+            feed = response.data.map(userGrouping => {
+                return userGrouping.photo.map(photo => {
+                    return <PhotoCard user={userGrouping.user} photo={photo}/>
+                })
+            }).reduce((a, b) => {
+                return a.concat(b);
+            })
+            window.setTimeout(() => {
+                this.setState({feed, feedSet: true})
+                console.log(feed);
+            }, 200)
+        });
+    }
     render() {
         return (
             <div className='feedView'>
-                <Nav />
-                <AddProfileImg />
-                <WelcomeCard />
-                <PhotoCard />
-                <Footer />
+                <Nav/>
+                <AddProfileImg/>
+                {this.state.feedSet && this.state.feed.length > 0 ? null : <WelcomeCard/> }
+                {this.state.feedSet ? this.state.feed : null}
+                <Footer/>
             </div>
         )
     }
