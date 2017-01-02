e27f63b79c4740d69575f0d5471e42dc81caf438
mr433 <max.rodewald@gmail.com>
2017-01-02 14:17:22 -0700

    css

--- public/views/Profile/ProfileInfo.js
+++ public/views/Profile/ProfileInfo.js
@@ -1,107 +1,135 @@
 import React from 'react';
-import { Link } from "react-router";
+import {Link} from "react-router";
 import getLoggedInUser from '../../utils/getLoggedInUser';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-
-export default class ProfileInfo extends React.Component{
-  constructor(props){
-    super(props);
-    this.state ={
-      showLogout: false,
+export default class ProfileInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showLogout: false
+        }
     }
-  }
 
-  toggleLogout(){
-    this.setState({
-      showLogout: !this.state.showLogout
-    });
-  }
+    toggleLogout() {
+        this.setState({
+            showLogout: !this.state.showLogout
+        });
+    }
 
-  logout(){
-    event.preventDefault();
-    Cookies.remove('user');
+    logout() {
+        event.preventDefault();
+        Cookies.remove('user');
+        window.location.href = '#/'
+    }
 
-    window.location.href = '#/'
-  }
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps, "this is the child");
+    }
 
-  componentWillMount(){
+    render() {
+        return (
 
-  }
-  componentWillReceiveProps(nextProps){
-    console.log(nextProps, "this is the child");
+            <div className='profileInfo'>
+                {this.state.showLogout
+                    ? <div className="logoutOverlay">
+                            <div>
+                                <ul className="logoutUl">
+                                    <li className="logoutLi">
+                                        <button onClick={this.logout.bind(this)} className="logoutButton">Log out</button>
+                                    </li>
+                                    <li className="logoutLi">
+                                        <button onClick={this.toggleLogout.bind(this)} className="logoutButton">Cancel</button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    : null}
+                <div className='profileInfoRow'>
+                    <img className="profilePhoto" src={this.props.user.profilepic}/>
+                    <div className="profileInfoRight">
+                        <div className='profileTopRow'>
+                            <span className='username'>{this.props.user.username}</span>
+                                {this.props.currentuser
+                                    ? <Link to="/editProfile">
+                                            <button className="hideOnMobile button buttonClear">Edit Profile</button>
+                                        </Link>
+                                    : null}
+                                {this.props.showfollow
+                                    ? <span className="button buttonClear" onClick={this.props.clickFollowHandler}>Follow</span>
 
-  }
-
-
-  render(){
-    return(
-
-      <div>
-        {this.state.showLogout ? <div className="logoutOverlay"> <div>
-        <ul className="logoutUl">
-        <li className="logoutLi">
-        <button onClick={this.logout.bind(this)} className="logoutButton">Log out</button>
-        </li>
-        <li className="logoutLi">
-        <button onClick={this.toggleLogout.bind(this)} className="logoutButton">Cancel</button>
-        </li>
-        </ul>
-        </div></div> : null}
-
-        <div className="profileInfoContainer">
-          <div className="profilePhotoBox">
-              <img className="profilePhoto" src={this.props.user.profilepic}/>
-          </div>
-          <div className="profileInfo">
-          <div className="profile-username">{this.props.user.username}
-
-            <div>
-              {this.props.currentuser ?
-              <Link to="/editProfile">
-              <button className="button">Edit Profile</button>
-            </Link>
-              : null}
-              {this.props.showfollow ?
-
-              <span className="button" onClick={this.props.clickFollowHandler}>Follow</span>
-
-              : null}
-              {this.props.showfollowing ?
-              <div>
-              <span className="button" onClick={this.props.clickUnfollowHandler}>Following</span>
-              <span><img className=" button followingarrow" src="http://image.flaticon.com/icons/png/512/60/60995.png"/></span>
-              </div>
-
-
-              : null}
+                                    : null}
+                                {this.props.showfollowing
+                                    ?<div className="following button" onClick={this.props.clickUnfollowHandler}>Following</div>
+                                    : null}
+                                {this.props.showfollowing
+                                    ?<div className='following button'>
+                                        <div className="arrowSprite"></div>
+                                    </div>
+                                    : null}
+                                <div onClick={this.toggleLogout.bind(this)} className='optionsSprite'></div>
+                            <div className={`appLinks ${this.state.showLogout
+                                ? "loginSpacing"
+                                : null}`}></div>
+                        </div>
+                        <Link className='showOnMobile' to="/editProfile">
+                            <button className="button buttonClear">Edit Profile</button>
+                        </Link>
+                        <div className='counters'>
+                            <span>
+                                <span className="count">{this.props.posts.length + ' '}
+                                </span>
+                                posts
+                            </span>
+                            <span>
+                                <span className="count">{this.props.user.followers
+                                        ? this.props.user.followers.length + ' '
+                                        : 0}</span>
+                                followers
+                            </span>
+                            <span>
+                                <span className="count">{this.props.user.following
+                                        ? this.props.user.following.length + ' '
+                                        : 0}</span>
+                                following
+                            </span>
+                        </div>
+                        <span className='bioContainer'>
+                            <span className='name'>
+                                {this.props.user.fullname}
+                            </span>
+                            {this.props.user.bio}
+                        </span>
+                    </div>
+                </div>
+                <span className='mobileBioContainer'>
+                    <span className='name'>
+                        {this.props.user.fullname}
+                    </span>
+                    {this.props.user.bio}
+                </span>
+                <div className='mobileCounters'>
+                    <span className='col'>
+                        <span className="label">{this.props.posts.length}
+                        </span>
+                        posts
+                    </span>
+                    <span className='col'>
+                        <span className="label">{this.props.user.followers
+                                ? this.props.user.followers.length + ' '
+                                : 0}</span>
+                        followers
+                    </span>
+                    <span className='col'>
+                        <span className="label">{this.props.user.following
+                                ? this.props.user.following.length + ' '
+                                : 0}</span>
+                        following
+                    </span>
+                </div>
             </div>
-            <div>
 
-              <button onClick={this.toggleLogout.bind(this)} className="button">...</button>
-
-            </div>
-
-            <div className={`appLinks ${this.state.showLogout ? "loginSpacing" : null}`}>
-            </div>
-
-          </div>
-
-          <div className="counters">
-            <ul>
-              <li><span className="bold-profile">{this.props.posts.length}</span> posts</li>
-              <li><span className="bold-profile">{this.props.user.followers ? this.props.user.followers.length : 0}</span> followers</li>
-              <li><span className="bold-profile">{this.props.user.following ? this.props.user.following.length : 0}</span> following</li>
-            </ul>
-          </div>
-          <div><span className="bold-profile">{this.props.user.fullname}</span>{this.props.user.bio}
-        </div>
-        </div>
-
-      </div>
-    </div>
-
-    )
-  }
+        )
+    }
 }
