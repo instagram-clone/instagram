ea22c097701b563a8e4bcb3a0010647795c6e6fc
mr433 <max.rodewald@gmail.com>
2017-01-02 17:50:27 -0700

    ok

--- public/views/Profile/ProfileInfo.js
+++ public/views/Profile/ProfileInfo.js
@@ -75,7 +75,9 @@ export default class ProfileInfo extends React.Component {
                                             </div>
                                         </span>
                                     : null}
-                                {this.props.current}<span onClick={this.toggleLogout.bind(this)} className='optionsSprite'></span>
+                                {this.props.currentuser ?
+                                    <span onClick={this.toggleLogout.bind(this)} className='optionsSprite'></span>
+                                    : null}
                             <div className={`appLinks ${this.state.showLogout
                                 ? "loginSpacing"
                                 : null}`}></div>
