ceee4ea24c9d3acf9d0b1dabcbb4e648776eec5b
mr433 <max.rodewald@gmail.com>
2017-01-02 17:42:25 -0700

    Revert "fixing bug with options button"
    This reverts commit 7549820e5ea0f6a850465a1e4c41b976e2ee3e7d.

--- public/views/Profile/ProfileInfo.js
+++ public/views/Profile/ProfileInfo.js
@@ -75,10 +75,7 @@ export default class ProfileInfo extends React.Component {
                                             </div>
                                         </span>
                                     : null}
-                            {this.props.currentuser
-                                ?
-                                    <span onClick={this.toggleLogout.bind(this)} className='optionsSprite'></span>
-                                :   null}
+                                {this.props.current}<span onClick={this.toggleLogout.bind(this)} className='optionsSprite'></span>
                             <div className={`appLinks ${this.state.showLogout
                                 ? "loginSpacing"
                                 : null}`}></div>
@@ -89,7 +86,6 @@ export default class ProfileInfo extends React.Component {
                               <Link className='showOnMobile' to="/editProfile">
                                 <button className="button buttonClear">Edit Profile</button>
                               </Link>
-                              <span onClick={this.toggleLogout.bind(this)} className='optionsSprite'></span>
                             </div>
                              : null}
                         <div className='counters'>
